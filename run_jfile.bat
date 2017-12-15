46fead650c7d76adef129e43a9057197c65cffd5
Alexis <alexischeungho@gmail.com>
2017-12-15 14:56:49 -0800

    Add script, fix merge

--- run_jfile.bat
+++ run_jfile.bat
@@ -1,6 +1,14 @@
 @Echo Off
-if exist PSL.class (
-    del PSL.class
+
+if "%~1"=="" (
+	Echo "No arguments were entered"
+) else (
+
+	cd src
+	javac -cp .;../Antlr/antlr-4.7-complete.jar PSL.java
+	java -cp .;../Antlr/antlr-4.7-complete.jar PSL %1.psl
+	
+	java -jar ../Jasmin/jasmin.jar PSL.j
+	java -cp .;../PascalRTL.jar PSL
+	cd ..
 )
-java -jar Jasmin/jasmin.jar %1.j
-java -cp .;PascalRTL.jar PSL
